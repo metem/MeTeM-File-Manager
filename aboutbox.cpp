7472875b75b4f3d200af191fdfba322d977729d5
Mateusz Ucher <mateusz.ucher@gmail.com>
2013-06-16 02:10:35 +0200

    Added COPYING file

--- aboutbox.cpp
+++ aboutbox.cpp
@@ -6,6 +6,11 @@ AboutBox::AboutBox(QWidget *parent) :
     ui(new Ui::AboutBox)
 {
     ui->setupUi(this);
+
+    QFile file("COPYING.txt");
+    if (file.open(QFile::ReadOnly | QFile::Text))
+        ui->plainTextEdit->appendPlainText(file.readAll());
+    else ui->plainTextEdit->appendPlainText("Can not load file.");
 }
 
 AboutBox::~AboutBox()
